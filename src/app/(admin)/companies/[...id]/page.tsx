1a66be8f2b2c02674e00ed73fb321d0b3aa51fac
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2024-07-12 23:59:38 +0300

    fonts-layout

---
+++ src/app/(admin)/companies/[...id]/page.tsx
@@ -0,0 +1,17 @@
+import Header from '@/app/components/header';
+import React from 'react';
+
+export interface PageProps {
+  params: { id: string[] }
+}
+
+export default function Page({params}: PageProps) {
+  return (
+    <>
+      <Header>Companies ({String(params.id)})</Header>
+      <p>
+        {new Date().toTimeString()}
+      </p>
+    </>
+  );
+}
---
+++ src/app/(admin)/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export interface PageProps {
+}
+
+export default function Page({}: PageProps) {
+  return (
+    <main>
+      <h1 className='txt-xl'>Dashboar page</h1>
+    </main>
+  );
+}
