a1206ccc70a396957239a007f8c7e8d3be33ad78
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2024-07-13 14:30:11 +0300

    dashboard

---
+++ src/app/(admin)/dashboard/@sales/page.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { getSummarySales } from '@/lib/api';
+import SummaryTable from '@/app/components/summary-tabel';
+import SummaryTableHeader from '@/app/components/summary-tabel-header';
+import SummaryTableCell from '@/app/components/summary-tabel-cell';
+import DashboardCard from '@/app/components/dashboard-card';
+
+export interface PageProps {}
+
+export default async function Page({}: PageProps) {
+  const data = await getSummarySales();
+
+  return (
+    <DashboardCard label="Sales details">
+      <SummaryTable
+        headers={
+          <>
+            <SummaryTableHeader>Company</SummaryTableHeader>
+            <SummaryTableHeader align="center">Sold</SummaryTableHeader>
+            <SummaryTableHeader align="center">Income</SummaryTableHeader>
+          </>
+        }
+      >
+        {data.map(({ companyId, companyTitle, sold, income }) => (
+          <tr key={companyId}>
+            <SummaryTableCell>{companyTitle}</SummaryTableCell>
+            <SummaryTableCell align="center">{sold}</SummaryTableCell>
+            <SummaryTableCell align="center">{`$${income}`}</SummaryTableCell>
+          </tr>
+        ))}
+      </SummaryTable>
+    </DashboardCard>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 import { headers } from "next/headers";
 import AddCompanyButton from "./components/add-company-button";
-import ServerComponent from "./components/server-componeny";
-import ClientComponent from "./components/client-component";
-import ServerComponentCopy from "./components/server-component-coppy";
+import MagicButton from "./components/magic-button";
 
 export default function Home() {
   console.log(headers());
@@ -10,10 +8,7 @@ export default function Home() {
     <main >
       <h1 className="text-xl">Colombo-colt {new Date().toTimeString()}</h1>
       <AddCompanyButton />
-      <ServerComponent />
-      <ClientComponent>
-        <ServerComponentCopy />
-      </ClientComponent>
+      <MagicButton />
     </main>
   );
 }
