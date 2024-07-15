23d5dc7eefb076d5bc04ae759d4db23cdf81c539
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2024-07-15 12:28:43 +0300

    avatar-remake

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -12,16 +12,8 @@ export default function Header({ children }: HeaderProps) {
         {children}
       </h1>
       <div className="w-px self-stretch bg-gray-300" />
-      <div className="flex gap-3 items-center"> {/* Добавлен класс items-center для центрирования по вертикали */}
-        <div className="relative rounded-full overflow-hidden" style={{ width: '44px', height: '44px' }}>
-          <Image
-            layout="fill"  // Заполнение родительского контейнера
-            objectFit="cover" // Заполнение и обрезка изображения
-            className="rounded-full" // Задаем скругление у изображения
-            src="/images/avatar.png"
-            alt="avatar"
-          />
-        </div>
+      <div className="flex gap-3">
+        <Image width={44} height={44} src="/images/avatar.png" alt="avatar" />
         <div>
           <p className="text-base	font-semibold text-gray-900">Adam Smith</p>
           <p className="text-sm	font-light text-gray-900">adamsmith@gmail.com</p>
