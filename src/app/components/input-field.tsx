2b0eba2e9b49b4af2e1cf5a40bbb34e4b494779f
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2024-07-15 14:10:02 +0300

    add-company-form

--- src/app/components/input-field.tsx
+++ src/app/components/input-field.tsx
@@ -1,22 +1,27 @@
-'use client'
+'use client';
 
 import React from 'react';
-import { Field } from 'formik';
+import { Field, FieldAttributes } from 'formik';
 
-export interface InputFieldPrors 
-extends React.InputHTMLAttributes<HTMLInputElement> {
+export interface InputFieldProps
+  extends React.InputHTMLAttributes<HTMLInputElement>,
+    Pick<FieldAttributes<string>, 'as'> {
   label?: string;
 }
 
-export default function InputField({ label, id, ...rest }: InputFieldPrors) {
+export default function InputField({ label, id, ...rest }: InputFieldProps) {
   return (
-    <div className='flex flex-col'>
+    <div className="flex flex-col">
       {label && (
-        <label htmlFor={id} className={'mb-2 text-base color-gray-900'}>
+        <label htmlFor={id} className="mb-2 text-base color-gray-900">
           {label}
-        </label>)
-      }
-      <Field {...rest} id={id} className='p-3 h-11 text-sm rounder border-gray-300 shadow' />
+        </label>
+      )}
+      <Field
+        {...rest}
+        id={id}
+        className="p-3 h-11 text-sm rounded border border-gray-300 shadow"
+      />
     </div>
-  )
+  );
 }
